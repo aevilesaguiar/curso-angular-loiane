3f0daeb0337c8a33a3f37c890b42b7b6158efdbe
aevilesaguiar <aeviles@hotmail.com>
2022-08-23 14:13:56 -0300

    aula 05

---
+++ curso-angular2/typescript/main.js
@@ -0,0 +1,22 @@
+var minhaVar = 'minha variavel';
+function minhaFuncao(x, y) {
+    return x + y;
+}
+var num = 2;
+var PI = 3.14;
+var numeros = [1, 2, 3];
+//esse código é igual ao de baixo;
+numeros.map(function (valor) {
+    return valor * 2;
+});
+numeros.map(function (valor) { return valor * 2; });
+var Matematica = /** @class */ (function () {
+    function Matematica() {
+    }
+    Matematica.prototype.soma = function (x, y) {
+        return x + y;
+    };
+    return Matematica;
+}());
+var n1 = 'sdsfsfsf';
+n1 = 4;
---
+++ curso-angular2/typescript/main.ts
@@ -0,0 +1,27 @@
+var minhaVar='minha variavel';
+
+function minhaFuncao(x,y){
+    return x+y;
+}
+
+
+let num =2;
+const PI=3.14;
+
+var numeros=[1,2,3];
+
+//esse código é igual ao de baixo;
+numeros.map(function(valor){
+    return  valor*2;
+})
+
+numeros.map(valor => valor*2);
+
+class Matematica {
+    soma(x,y){
+        return x+y;
+    }
+}
+
+var n1: any ='sdsfsfsf';
+n1=4;
