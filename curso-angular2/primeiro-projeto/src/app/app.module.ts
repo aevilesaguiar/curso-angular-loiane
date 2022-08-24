d08713aa69b9f9b4ff01ffa7aa67070a0d2d4b79
aevilesaguiar <aeviles@hotmail.com>
2022-08-24 08:48:55 -0300

    inserindo aula 07 - modulo serviços

--- curso-angular2/primeiro-projeto/src/app/app.module.ts
+++ curso-angular2/primeiro-projeto/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
   declarations: [
@@ -14,9 +15,10 @@ import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CursosModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [],//colocamos os serviços no providers
+  bootstrap: [AppComponent] //quando trabalhamos com angular nós trbalhamos com SPA , nós instanciamos app.component.html, onde chamamos a rota, menu ...
 })
 export class AppModule { }
