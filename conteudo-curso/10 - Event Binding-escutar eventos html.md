847f80a71aff4fb3b14d10fea0454d2c2941c0db
aevilesaguiar <aeviles@hotmail.com>
2022-08-24 21:10:28 -0300

    finalizando event binding

--- conteudo-curso/10 - Event Binding-escutar eventos html.md
+++ conteudo-curso/10 - Event Binding-escutar eventos html.md
@@ -1,2 +1,6 @@
-﻿## Event Binding(Escutar eventos no html- Binding de eventos)
+﻿## Event Binding(Escutar eventos no html do nosso template e executar uma lógica - Binding de eventos)
 
+
+## Referencias
+
+- https://developer.mozilla.org/en-US/docs/Web/API/Element/click_event
--- curso-angular2/data-binding2/src/app/data-binding/data-binding.component.ts
+++ curso-angular2/data-binding2/src/app/data-binding/data-binding.component.ts
@@ -3,7 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.css']
+  styleUrls: ['./data-binding.component.css'],
+  styles:[
+    `.highlight{
+      background-color: yellow;
+      font-weight: bold;
+    }`
+  ]
 })
 export class DataBindingComponent implements OnInit {
 
@@ -14,6 +20,12 @@ export class DataBindingComponent implements OnInit {
   urlImage: any='https://i.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI';
   urlImage2: any='https://i.picsum.photos/id/900/200/300.jpg?grayscale&hmac=8TpxSagv28YIsRXXnI_hDPAkCl73vKYNznki9MBiBHo';
 
+  valorAtual:string='';
+
+  valorSalvo:any = '';
+
+  isMouseOver: boolean=false;
+
 
   getValor(){
     return 10;
@@ -23,6 +35,21 @@ export class DataBindingComponent implements OnInit {
     return true;
   }
 
+  botaoClicado(){
+    alert('Botão clicado!');
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    this.valorAtual=(<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valor:any){
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut(){
+this.isMouseOver=!this.isMouseOver;
+  }
 
   constructor() { }
 
